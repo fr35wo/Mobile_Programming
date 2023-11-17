b3c7a08019136e0e0163abdfee54b53daec40132
seokhyeon <114850357+fr35wo@users.noreply.github.com>
2023-11-17 20:21:13 +0900

    Add files via upload

---
+++ TableLayoutTest/src/main/java/com/example/table_layout_test/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.table_layout_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_stretched);
+    }
+
+}
